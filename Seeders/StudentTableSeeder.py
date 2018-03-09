f4aa0da1edb41cc9708747d136b5caf3f15104f0
hossein moghadam <hosseinghmoghadam@gmail.com>
2018-03-10 01:27:51 +0330

    edit student seeder

--- Seeders/StudentTableSeeder.py
+++ Seeders/StudentTableSeeder.py
@@ -28,17 +28,17 @@ def student_seed():
 
     student = Student()
     student.firstname = 'علی'
-    student.lastname = 'مقدم'
-    student.father = 'فرزاد'
-    student.brithday = '1374-02-20'
+    student.lastname = 'یداللهی'
+    student.father = 'عظاالدین'
+    student.brithday = '1374-09-23'
     student.location_brith = 'طهران'
-    student.phone = '0938094882'
-    student.mobile = '02623بب588'
+    student.phone = '09137304084'
+    student.mobile = '02623588'
     student.national_code = '4321'
     student.status = 'active'
     student.entry_semester = '1396-تیر'
     student.img = 'ندارد'
-    student.address = 'قاین'
+    student.address = 'اصفهان'
     student.student_number = 3963002
     student.id = 2
     student.password = bcrypt.hash('1234')
