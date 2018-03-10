85a605017757bf95f07f07c3edcb1203ed6abe86
hossein moghadam <hosseinghmoghadam@gmail.com>
2018-03-10 16:33:34 +0330

    bug fix name

--- routes.py
+++ routes.py
@@ -8,7 +8,7 @@ def route(api):
     api.add_resource(ChoiceCourseController.List, '/choicecourses', endpoint='choice_course_list')
     api.add_resource(ChoiceCourseController.Update, '/choicecourses', endpoint='update_choice_courses')
     api.add_resource(ChoiceCourseController.Delete, '/choicecourses', endpoint='delete_choice_courses')
-    api.add_resource(ChoiceCourseController.Store, '/choicecourses', endpoint='create_choice_course')
+    api.add_resource(ChoiceCourseController.Store, '/choicecourse', endpoint='create_choice_course')
     api.add_resource(ChoiceCourseController.Destroy, '/choicecourses/<int:choice_course_id>',
                      endpoint='delete_choice_course')
     api.add_resource(ScheduleController.List, '/schedule', endpoint='schedule')
